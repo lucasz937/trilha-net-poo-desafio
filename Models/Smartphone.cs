609feeffb33fc7e916f018e2858c4be22ad51f22
LucasVinicius <lucassvinicius937@gmail.com>
2024-01-04 10:12:08 -0300

    Desafio celular POO concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,17 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+       public string Numero { get; set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public string Memoria { get; private set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia (numero:"128289129", modelo : "Modelo N1", imei: "8192192", memoria: "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("edX");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero : "28182183", modelo: "Modelo X", imei: "37173721", memoria: "256");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
